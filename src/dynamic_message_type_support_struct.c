69d251860e2048f8164e1fca9a3033c76ae13305
methylDragon <methylDragon@gmail.com>
2023-04-09 06:02:42 -0700

    Refactor dynamic message type support impl to use allocators
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- src/dynamic_message_type_support_struct.c
+++ src/dynamic_message_type_support_struct.c
@@ -24,82 +24,143 @@
 #include "rosidl_dynamic_typesupport/dynamic_message_type_support_struct.h"
 #include "rosidl_dynamic_typesupport/identifier.h"
 
-
 rcutils_ret_t
-rosidl_dynamic_message_type_support_handle_create(
+rosidl_dynamic_message_type_support_handle_init(
   rosidl_dynamic_typesupport_serialization_support_t * serialization_support,
   const rosidl_type_hash_t * type_hash,
   const rosidl_runtime_c__type_description__TypeDescription * type_description,
   const rosidl_runtime_c__type_description__TypeSource__Sequence * type_description_sources,
-  rosidl_message_type_support_t ** ts)
+  rcutils_allocator_t * allocator,
+  rosidl_message_type_support_t * ts)
 {
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(serialization_support, RCUTILS_RET_INVALID_ARGUMENT);
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(type_hash, RCUTILS_RET_INVALID_ARGUMENT);
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(type_description, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(allocator, RCUTILS_RET_INVALID_ARGUMENT);
   RCUTILS_CHECK_ARGUMENT_FOR_NULL(ts, RCUTILS_RET_INVALID_ARGUMENT);
 
   // NOTE(methylDragon): Not supported for now
   // RCUTILS_CHECK_ARGUMENT_FOR_NULL(type_description_sources, RCUTILS_RET_INVALID_ARGUMENT);
 
   rcutils_ret_t ret = RCUTILS_RET_ERROR;
-  rcutils_allocator_t allocator = rcutils_get_default_allocator();
-  *ts = allocator.zero_allocate(1, sizeof(rosidl_message_type_support_t), allocator.state);
-  if (!*ts) {
-    RCUTILS_SET_ERROR_MSG("Could not allocate rosidl_message_type_support_t struct");
-    return RCUTILS_RET_BAD_ALLOC;
-  }
 
-  (*ts)->data = allocator.zero_allocate(
-    1, sizeof(rosidl_dynamic_message_type_support_impl_t), allocator.state);
+  ts->typesupport_identifier = rosidl_dynamic_typesupport_c__identifier;
+  ts->func = get_message_typesupport_handle_function;
+  ts->get_type_hash_func =
+    rosidl_get_dynamic_message_type_support_type_hash_function;
+  ts->get_type_description_func =
+    rosidl_get_dynamic_message_type_support_type_description_function;
+  ts->get_type_description_sources_func =
+    rosidl_get_dynamic_message_type_support_type_description_sources_function;
+
+  ts->data = allocator->zero_allocate(
+    1, sizeof(rosidl_dynamic_message_type_support_impl_t), allocator->state);
   if (!ts) {
-    RCUTILS_SET_ERROR_MSG("Could not allocate rosidl_dynamic_message_type_support_impl_t struct");
+    RCUTILS_SET_ERROR_MSG("Could not allocate dynamic message type support impl");
     ret = RCUTILS_RET_BAD_ALLOC;
     goto fail;
   }
 
   rosidl_dynamic_message_type_support_impl_t * ts_impl =
-    (rosidl_dynamic_message_type_support_impl_t *) (*ts)->data;
+    (rosidl_dynamic_message_type_support_impl_t *) ts->data;
 
-  // Copy init type hash (never null (checked above))
-  ts_impl->type_hash = allocator.zero_allocate(1, sizeof(rosidl_type_hash_t), allocator.state);
-  if (!ts_impl->type_hash) {
-    RCUTILS_SET_ERROR_MSG("Could not create type hash to assign into");
-    ret = RCUTILS_RET_BAD_ALLOC;
+  ret = rosidl_dynamic_message_type_support_handle_impl_init(
+    serialization_support, type_hash, type_description, type_description_sources, allocator,
+    ts_impl);
+  if (ret != RCUTILS_RET_OK)
+  {
+    RCUTILS_SET_ERROR_MSG_AND_APPEND_PREV_ERROR("Could not init dynamic message type support impl");
     goto fail;
   }
 
-  ts_impl->type_hash->version = type_hash->version;
-  memcpy(ts_impl->type_hash->value, type_hash->value, sizeof(type_hash->value));
+  return RCUTILS_RET_OK;
 
-  // Copy init type description (never null (checked above))
-  ts_impl->type_description = rosidl_runtime_c__type_description__TypeDescription__create();
-  if (ts_impl->type_description == NULL) {
-    RCUTILS_SET_ERROR_MSG("Could not create type description to assign into");
-    ret = RCUTILS_RET_BAD_ALLOC;
-    goto fail;
+fail:
+  if (rosidl_dynamic_message_type_support_handle_fini(ts) != RCUTILS_RET_OK) {
+    RCUTILS_SAFE_FWRITE_TO_STDERR_AND_APPEND_PREV_ERROR(
+      "While handling another error, could not finalize dynamic message type support handle");
+  }
+  return ret;
+}
+
+rcutils_ret_t
+rosidl_dynamic_message_type_support_handle_fini(rosidl_message_type_support_t * ts)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(ts, RCUTILS_RET_INVALID_ARGUMENT);
+
+  if (ts->typesupport_identifier != rosidl_dynamic_typesupport_c__identifier) {
+    RCUTILS_SET_ERROR_MSG("Type support not from this implementation");
+    return RCUTILS_RET_INVALID_ARGUMENT;
   }
 
+  // NOTE(methylDragon): Ignores const...
+  rosidl_dynamic_message_type_support_impl_t * ts_impl =
+    (rosidl_dynamic_message_type_support_impl_t *)ts->data;
+  rcutils_allocator_t allocator = ts_impl->allocator;
+
+  rcutils_ret_t ret = rosidl_dynamic_message_type_support_handle_impl_fini(ts_impl);
+  if (ret != RCUTILS_RET_OK) {
+    RCUTILS_SET_ERROR_MSG_AND_APPEND_PREV_ERROR(
+      "Could not finalize dynamic type support handle impl");
+    return ret;
+  }
+
+  allocator.deallocate((void *)ts->data, &allocator.state);
+  return RCUTILS_RET_OK;
+}
+
+rcutils_ret_t
+rosidl_dynamic_message_type_support_handle_impl_init(
+  rosidl_dynamic_typesupport_serialization_support_t * serialization_support,
+  const rosidl_type_hash_t * type_hash,
+  const rosidl_runtime_c__type_description__TypeDescription * type_description,
+  const rosidl_runtime_c__type_description__TypeSource__Sequence * type_description_sources,
+  rcutils_allocator_t * allocator,
+  rosidl_dynamic_message_type_support_impl_t * ts_impl)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(serialization_support, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(type_hash, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(type_description, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(allocator, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(ts_impl, RCUTILS_RET_INVALID_ARGUMENT);
+
+  // NOTE(methylDragon): Not supported for now
+  // RCUTILS_CHECK_ARGUMENT_FOR_NULL(type_description_sources, RCUTILS_RET_INVALID_ARGUMENT);
+
+  rcutils_ret_t ret = RCUTILS_RET_ERROR;
+
+  // allocator
+  ts_impl->allocator = *allocator;
+
+  // type_hash
+  ts_impl->type_hash.version = type_hash->version;
+  memcpy(ts_impl->type_hash.value, type_hash->value, sizeof(type_hash->value));
+
+  // type_description
+  if (!rosidl_runtime_c__type_description__TypeDescription__init(&ts_impl->type_description)) {
+    RCUTILS_SET_ERROR_MSG("Could not init type description");
+    ret = RCUTILS_RET_NOT_INITIALIZED;
+    goto fail;
+  }
   if (!rosidl_runtime_c__type_description__TypeDescription__copy(
-      type_description, ts_impl->type_description))
+      type_description, &ts_impl->type_description))
   {
     RCUTILS_SET_ERROR_MSG("Could not copy type description");
     ret = RCUTILS_RET_ERROR;
     goto fail;
   }
 
-  // Copy init type description sources (might be null)
+  // type_description_sources
+  if (!rosidl_runtime_c__type_description__TypeSource__Sequence__init(
+      &ts_impl->type_description_sources, 0))
+  {
+    RCUTILS_SET_ERROR_MSG("Could not init type description sources");
+    ret = RCUTILS_RET_NOT_INITIALIZED;
+    goto fail;
+  }
   if (type_description_sources != NULL) {
-    ts_impl->type_description_sources =
-      rosidl_runtime_c__type_description__TypeSource__Sequence__create(
-      type_description_sources->size);
-    if (ts_impl->type_description_sources == NULL) {
-      RCUTILS_SET_ERROR_MSG("Could not create type description sources sequence to assign into");
-      ret = RCUTILS_RET_BAD_ALLOC;
-      goto fail;
-    }
-
     if (!rosidl_runtime_c__type_description__TypeSource__Sequence__copy(
-        type_description_sources, ts_impl->type_description_sources))
+        type_description_sources, &ts_impl->type_description_sources))
     {
       RCUTILS_SET_ERROR_MSG("Could not copy type description sources");
       ret = RCUTILS_RET_ERROR;
@@ -109,81 +170,52 @@ rosidl_dynamic_message_type_support_handle_create(
 
   ts_impl->serialization_support = serialization_support;
 
-  (*ts)->typesupport_identifier = rosidl_dynamic_typesupport_c__identifier;
-  (*ts)->func = get_message_typesupport_handle_function;
-  (*ts)->get_type_hash_func =
-    rosidl_get_dynamic_message_type_support_type_hash_function;
-  (*ts)->get_type_description_func =
-    rosidl_get_dynamic_message_type_support_type_description_function;
-  (*ts)->get_type_description_sources_func =
-    rosidl_get_dynamic_message_type_support_type_description_sources_function;
-
+  // dynamic_message_type
   ret = rosidl_dynamic_typesupport_dynamic_type_create_from_description(
     ts_impl->serialization_support, type_description, &ts_impl->dynamic_message_type);
-  if (ret != RCUTILS_RET_OK || !ts_impl->dynamic_message_type) {
+  if (ret != RCUTILS_RET_OK) {
     RCUTILS_SET_ERROR_MSG(
       "Could not construct dynamic type for rosidl_dynamic_message_type_support_impl_t struct");
     goto fail;
   }
 
+  // dynamic_message
   ret = rosidl_dynamic_typesupport_dynamic_data_create_from_dynamic_type(
     ts_impl->dynamic_message_type, &ts_impl->dynamic_message);
-  if (ret != RCUTILS_RET_OK || !ts_impl->dynamic_message) {
-    rcutils_error_string_t error_string = rcutils_get_error_string();
-    rcutils_reset_error();
-    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING(
-      "Could not construct dynamic data for rosidl_dynamic_message_type_support_impl_t struct:\n%s",
-      error_string.str);
+  if (ret != RCUTILS_RET_OK) {
+    RCUTILS_SET_ERROR_MSG_AND_APPEND_PREV_ERROR(
+      "Could not construct dynamic data for rosidl_dynamic_message_type_support_impl_t struct");
     goto fail;
   }
 
   return RCUTILS_RET_OK;
 
 fail:
-  if (rosidl_dynamic_message_type_support_handle_destroy(*ts) != RCUTILS_RET_OK) {
+  if (rosidl_dynamic_message_type_support_handle_impl_fini(ts_impl) != RCUTILS_RET_OK) {
     RCUTILS_SAFE_FWRITE_TO_STDERR(
-      "While handling another error, could not destroy dynamic message typesupport");
+      "While handling another error, could not finalize dynamic message type support handle impl");
   }
   return ret;
 }
 
-
 rcutils_ret_t
-rosidl_dynamic_message_type_support_handle_destroy(rosidl_message_type_support_t * ts)
+rosidl_dynamic_message_type_support_handle_impl_fini(
+  rosidl_dynamic_message_type_support_impl_t * ts_impl)
 {
-  RCUTILS_CHECK_ARGUMENT_FOR_NULL(ts, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(ts_impl, RCUTILS_RET_INVALID_ARGUMENT);
 
-  // NOTE(methylDragon): Ignores const...
-  if (ts->typesupport_identifier != rosidl_dynamic_typesupport_c__identifier) {
-    RCUTILS_SET_ERROR_MSG("type support not from this implementation");
-    return RCUTILS_RET_INVALID_ARGUMENT;
+  rosidl_runtime_c__type_description__TypeDescription__fini(&ts_impl->type_description);
+  rosidl_runtime_c__type_description__TypeSource__Sequence__fini(
+    &ts_impl->type_description_sources);
+  if (ts_impl->dynamic_message_type) {
+    rosidl_dynamic_typesupport_dynamic_type_destroy(ts_impl->dynamic_message_type);
+  }
+  if (ts_impl->dynamic_message) {
+    rosidl_dynamic_typesupport_dynamic_data_destroy(ts_impl->dynamic_message);
   }
 
-  rcutils_allocator_t allocator = rcutils_get_default_allocator();
-
-  if (ts->data) {
-    rosidl_dynamic_message_type_support_impl_t * ts_impl =
-      (rosidl_dynamic_message_type_support_impl_t *)ts->data;
-    if (ts_impl->type_description) {
-      rosidl_runtime_c__type_description__TypeDescription__fini(ts_impl->type_description);
-    }
-    if (ts_impl->type_description_sources) {
-      rosidl_runtime_c__type_description__TypeSource__Sequence__fini(
-        ts_impl->type_description_sources);
-    }
-    if (ts_impl->dynamic_message_type) {
-      rosidl_dynamic_typesupport_dynamic_type_destroy(ts_impl->dynamic_message_type);
-    }
-    if (ts_impl->dynamic_message) {
-      rosidl_dynamic_typesupport_dynamic_data_destroy(ts_impl->dynamic_message);
-    }
-    allocator.deallocate(ts_impl->type_hash, allocator.state);
-  }
-  allocator.deallocate((void *)ts->data, allocator.state);
-  allocator.deallocate(ts, allocator.state);
   return RCUTILS_RET_OK;
 }
-
 
 // GETTERS =========================================================================================
 const rosidl_type_hash_t *
@@ -197,9 +229,8 @@ rosidl_get_dynamic_message_type_support_type_hash_function(
   }
   rosidl_dynamic_message_type_support_impl_t * ts_impl =
     (rosidl_dynamic_message_type_support_impl_t *) ts->data;
-  return ts_impl->type_hash;
+  return &ts_impl->type_hash;
 }
-
 
 const rosidl_runtime_c__type_description__TypeDescription *
 rosidl_get_dynamic_message_type_support_type_description_function(
@@ -212,9 +243,8 @@ rosidl_get_dynamic_message_type_support_type_description_function(
   }
   rosidl_dynamic_message_type_support_impl_t * ts_impl =
     (rosidl_dynamic_message_type_support_impl_t *) ts->data;
-  return ts_impl->type_description;
+  return &ts_impl->type_description;
 }
-
 
 const rosidl_runtime_c__type_description__TypeSource__Sequence *
 rosidl_get_dynamic_message_type_support_type_description_sources_function(
@@ -227,5 +257,5 @@ rosidl_get_dynamic_message_type_support_type_description_sources_function(
   }
   rosidl_dynamic_message_type_support_impl_t * ts_impl =
     (rosidl_dynamic_message_type_support_impl_t *) ts->data;
-  return ts_impl->type_description_sources;
+  return &ts_impl->type_description_sources;
 }
